66dd56239741cb3ff9e12c0a092a8cf68cc6d83a
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-11-26 07:33:45 -0800

    Add valid anagram solution

--- ruby-solutions/length_of_longest_substring.rb
+++ ruby-solutions/length_of_longest_substring.rb
@@ -1,30 +1,68 @@
-def length_of_longest_substring(s)
-    p 0 if s == nil || s.length == 0
+# Given a string, find the length of the longest substring without repeating characters.
 
-    map = {}
-    length = 0
-    max_length = length
-    start = 0
+# Examples:
 
-    # # scan from left to right
-    until start >= s.length
-       i = start
-       c = s[i]
+# Given "abcabcbb", the answer is "abc", which the length is 3.
 
-       if map[c] != nil && map[c] >= start
-        start = map[c] + 1 # start new search with repeated word's last location + 1
-        length = i - start # real length -> from for example 3 to 5 is 3, so it's 5-3+1 and + 1 happens later
-       end
+# Given "bbbbb", the answer is "b", with the length of 1.
 
-       length+=1 # real length -> from for example 3 to 5 is 3, so it's 5-3+1 and + 1 happens later
-       # set the longest substring
-       max_length = length if length > max_length
-       map[c] = i
-       start = start + 1
-    end
-    p max_length
+# Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+# def length_of_longest_substring(s)
+#     return 0 if s == nil || s.length == 0
+
+#     map = {}
+#     length = 0
+#     max_length = length
+#     start = 0
+
+#     # # scan from left to right
+#     until start >= s.length
+#        i = start
+#        c = s[i]
+
+#        if map[c] != nil && map[c] >= start
+#         start = map[c] + 1 # start new search with repeated word's last location + 1
+#         length = i - start # real length -> from for example 3 to 5 is 3, so it's 5-3+1 and + 1 happens later
+#        end
+
+#        length+=1 # real length -> from for example 3 to 5 is 3, so it's 5-3+1 and + 1 happens later
+#        # set the longest substring
+#        max_length = length if length > max_length
+#        map[c] = i
+#        start+=1
+#     end
+#     return max_length
     
+# end
+
+def length_of_longest_substring(s)
+
+  return 0 if s == nil || s.length == 0 
+  
+  ans = {}
+  count = 0
+  greedy_count = 0
+
+  i = 0
+  j = s.length
+
+  while i < j
+    if !ans[s[i]]
+      ans[s[i]] = 1
+      p ans
+      count += 1
+    else
+      ans[s[i]] += 1
+      p ans
+    end
+      i+=1
+  end
+  greedy_count = count if count > greedy_count
+  p greedy_count
 end
 
-length_of_longest_substring("")
+# length_of_longest_substring("")
 length_of_longest_substring("abcabccc")
+# length_of_longest_substring("bbbbb")
+length_of_longest_substring("pwwkew")
