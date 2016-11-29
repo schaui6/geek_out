49e87f45add8ba4d576f3d53f90ef40496731531
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-11-28 20:38:58 -0800

    Fix bug for marklar

--- ruby-solutions/marklar.rb
+++ ruby-solutions/marklar.rb
@@ -10,17 +10,20 @@ def marklar(str)
     if word.length > 4
       if word === word.capitalize
         word = 'Marklar'
+        result.push(word)
       elsif /(\W)/ === word
         special_char = word[-1]
         word = "marklar#{special_char}"
+        result.push(word)
       else
         word = 'marklar'
+        result.push(word)
       end
+    else
+      result.push(word)
     end
-  
-    result.push(word)
   end
-    p result.join(' ')
+    return result.join(' ')
 end
 
 marklar("The quick brown fox")
