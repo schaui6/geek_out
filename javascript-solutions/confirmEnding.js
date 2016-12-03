d4c4f58486e43d2195067f543b2f7a3d89d163e8
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-12-02 18:14:18 -0800

    Add solution for confirmEnding.js

---
+++ javascript-solutions/confirmEnding.js
@@ -0,0 +1,25 @@
+function confirmEnding(str, target) {
+
+  if (str.split(" ").length > 1){
+    var lastChar = str.split(" ");
+    var lastWord = lastChar[lastChar.length - 1];
+
+    if (target.length !== lastWord.length){
+      console.log(lastWord.slice(lastWord.length - target.length)  === target);
+    }
+    else{
+      console.log(lastChar[lastChar.length - 1] === target);
+    }
+  }
+  else{
+    console.log(str[str.length -1] === target);
+  }
+}
+
+confirmEnding("Bastian", "n");
+confirmEnding("Connor", "n");
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification");
+confirmEnding("He has to give me a new name", "name");
+confirmEnding("Open sesame", "same");
+confirmEnding("Open sesame", "pen");
+confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain");
