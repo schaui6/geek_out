37500b6c5817299ca7ebe3eee5af8344fcbdff01
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-12-03 20:55:27 -0800

    Add solution for mutations.js

---
+++ javascript-solutions/mutations.js
@@ -0,0 +1,10 @@
+function mutation(arr) {
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  for (i=0;i<test.length;i++) {
+    if (target.indexOf(test[i]) < 0)
+      return false;
+  }
+  return true;
+ }
+mutation(["hello", "hey"]);
