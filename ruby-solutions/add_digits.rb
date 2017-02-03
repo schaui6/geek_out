096e6ec2f9e25782c6a9c720ef5faf8c1b63cba6
Sanderfer Chau <sanderfer.chau@gmail.com>
2017-02-03 13:32:46 -0800

    Add alternative solution

--- ruby-solutions/add_digits.rb
+++ ruby-solutions/add_digits.rb
@@ -1,15 +1,26 @@
 # @param {Integer} num
 # @return {Integer}
+
+
+# def add_digits(num)
+#   while num >= 10
+#     temp = 0
+#     while num > 0
+#       temp += num % 10
+#       num /= 10
+#     end
+#     num = temp
+#   end
+#   return num
+# end
+
 def add_digits(num)
+  num if num.to_s.length <= 1
+  
   while num >= 10
-    temp = 0
-    while num > 0
-      temp += num % 10
-      num /= 10
-    end
-    num = temp
+    num = num.to_s.split("").map {|i| i.to_i}.inject {|sum, n| sum.to_i += n.to_i}
   end
-  return num
+  num
 end
 
 p add_digits(38)
