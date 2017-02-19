733b948e07dd4b1c9b8ba342d82807e81444e562
Sanderfer Chau <sanderfer.chau@gmail.com>
2017-02-19 00:24:37 -0800

    Add sollution to sum all

--- js-notes/sumAll.js
+++ js-notes/sumAll.js
@@ -1,11 +1,30 @@
 function sumAll(arr) {
   var total = 0;
-  var len = arr.length;
+
   
-  for(var i = 0; i <= len;i++){
-    total += arr[i];
-  }
+  createRange(arr).forEach(function(val){
+    total += val;
+  });
+  
   return total;
+  
 }
 
-sumAll([1, 4]);
+function createRange(arr) {
+  var end = arr[1];
+  var start = arr[0];
+  var range = [];
+
+  if (arr[0] > arr[1]) {
+    start = arr[1];
+    end = arr[0];
+  }
+
+  for(var i = start; i <= end; i++){
+    range.push(i);
+  }
+  return range;
+}
+
+
+console.log(sumAll([10, 5]));
