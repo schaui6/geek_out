9117cf3efa3d672718187255d8c68e570091a95f
Sanderfer Chau <sanderfer.chau@gmail.com>
2017-02-19 01:56:53 -0800

    Add solution to whatIsInAName

---
+++ js-notes/whatIsInAName.js
@@ -0,0 +1,14 @@
+function whatIsInAName(collection, source) {
+  var srcKeys = Object.keys(source);
+
+  return collection.filter(function(obj) {
+    for(var i = 0; i < srcKeys.length; i++){
+      if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
