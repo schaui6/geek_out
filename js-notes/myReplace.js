18725a45a29fd71773894a1663bfaca4c35760d1
Sanderfer Chau <sanderfer.chau@gmail.com>
2017-02-19 15:36:13 -0800

    Add solution for myReplace.js

---
+++ js-notes/myReplace.js
@@ -0,0 +1,12 @@
+
+function myReplace(str, before, after) {
+  var index = str.indexOf(before);
+
+  if (str[index] === str[index].toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  }
+  str = str.replace(before, after);
+  return str;
+}
+
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
