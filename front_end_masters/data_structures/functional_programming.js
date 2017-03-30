a757b6f3c566687ac89d3371f2bd9d2b00fa3b1a
Sanderfer Chau <sanderfer.chau@gmail.com>
2017-03-30 14:24:26 -0700

    Add map and filter methods

--- front_end_masters/data_structures/functional_programming.js
+++ front_end_masters/data_structures/functional_programming.js
@@ -16,3 +16,46 @@ const myMap = (array, fn) => {
 };
 
 // reduce
+const addTogether = list => {
+  return list.reduce((acc, num)) => acc+num, 0;
+};
+
+const concatenateStringsWithSpaces =  list => {
+  return list.reduce((acc, string)) => acc + string + ' ', '');
+};
+
+const squaresAndSubtracts = list => {
+  return list
+    .map( num => num*num )
+    .reduce( (accumulator, num) => accumulator-num );
+};
+
+const myReduce = (list, fn, seed) => {
+  let answer = seed;
+  for (let i = 0; i < list.length; i++) {
+    answer = fun(answer, list[i]);
+  }
+  return answer;
+};
+
+// filter
+const filterOutOdds = nums => nums.filter( num => num % 2 === 0);
+
+const filterState = (list, state) => list.filter(person => person.state === state);
+
+const showOutOfCADevs = list => {
+  return list
+    .filter( person => person.state != 'CA')
+    .map (person => person.name.toUpperCase())
+    .reduce( (acc, nume) => '${acc}, ${name}');
+  };
+
+  const myFilter = (list,fn) => {
+    const answer = [];
+    for (let i=0; i < list.length; i++) {
+      if (fn(list[i])) {
+        answer.push(list[i]);
+      }
+    }
+    return answer;
+  };
